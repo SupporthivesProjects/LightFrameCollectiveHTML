1815c9af9a507a12f2638120ac84e2869604052b
SUPPORT HIVES <supporthives@SUPPORTs-MacBook-Air.local>
2026-06-15 15:50:36 +0530

    rahul

--- index.php
+++ index.php
@@ -1,3 +1,29 @@
 <?php include 'includes/header.php'; ?>
 
+<!-- Home S1 Start -->
+<section class="home_s1">
+    <div class="home_c1">
+        <div class="home_s1top">
+            <img src="./img/home_s1img.png">
+            <div class="home_s1titlebar">
+                <h1 class="home_s1title">Discover <span class="home_s1titlepurple">&</span> Download<br>Exceptional Imagery</h1>
+                <p class="home_s1subtitle">Explore our vast collection of high-quality stock images for personal and commercial use.<br>
+                Browse and download from our extensive library to find the perfect visuals for your projects.</p>
+            </div>
+        </div>
+        <div class="home_s1bottom">
+            <div class="home_s1bottomin">
+                <div class="home_s1bottomintop w-100">
+                    <div class="home_s1bottomintopleft">
+                        <img src="./img/home_s1bottomintopleftimg.png">
+                        <p class="home_s1bottomintoplefttitle">Quick Search</p>
+                    </div>
+                </div>
+                <p class="home_s1bottominbottom"><img src="./img/home_S1shield.svg">Curated, royalty-free images<span class="home_s1dot"></span>High resolution<span class="home_s1dot"></span>Commercial use ready</p>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- Home S1 End -->
+
 <?php include 'includes/footer.php'; ?>
