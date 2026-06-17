c41d92f124452bf2862e6892c185f1baf41c243b
SUPPORT HIVES <supporthives@SUPPORTs-MacBook-Air.local>
2026-06-17 13:22:08 +0530

    rahul

--- includes/header.php
+++ includes/header.php
@@ -39,7 +39,7 @@
           <div class="container">
             <div class="menu-logo">
                 <div class="menu-logo-mo">
-                    <a class="navbar-brand" href="#">
+                    <a class="navbar-brand" href="./index.php">
                     <img src="./img/logo.svg" alt="" class="img-fluid d-none d-md-none d-lg-block">
                     <img src="./img/logo-mo.svg" alt="" class="img-fluid d-lg-none d-md-block d-block">
                   </a>
@@ -96,7 +96,7 @@
                   <a class="nav-link" href="#">Pricing & Perks</a>
                 </li>
                 <li class="nav-item">
-                  <a class="nav-link" href="#">About</a>
+                  <a class="nav-link" href="./aboutus.php">About</a>
                 </li>
                 <li class="nav-item">
                   <a class="nav-link">FAQ’s</a>
