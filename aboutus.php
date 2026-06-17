694a9bb273787f5a3e64311d7008ab68f4f85444
SUPPORT HIVES <supporthives@SUPPORTs-MacBook-Air.local>
2026-06-17 15:30:37 +0530

    rahul

--- aboutus.php
+++ aboutus.php
@@ -1,5 +1,12 @@
 <?php include 'includes/header.php'; ?>
-
+<style>
+    .evlated-section{
+        background: #0F0F0F
+    }
+    .evlated-section p{
+        color: #FFFFFF;
+    }
+</style>
 <!-- ABOUT S1 Start -->
 <section class="abt_s1">
     <div class="abt_c1 container">
@@ -8,9 +15,11 @@
                 <h2 class="abt_s1title">About Us</h2>
                 <p class="abt_s1subtitle">We simplify the search for high-quality stock images. Our curated library, created by talented global contributors, offers diverse visuals to meet your needs. We are committed to affordable, royalty-free images and exceptional customer service.</p>
             </div>
-            <div class="searchboxmain">
-                <img class="search_img" src="./img/search.svg">
-                <input type="email" class="form-control abt_s1textbox" id="" placeholder="Search high-quality images...">
+            <div class="searchboxmainout">
+                <div class="searchboxmain">
+                    <img class="search_img" src="./img/search.svg">
+                    <input type="email" class="form-control abt_s1textbox" id="" placeholder="Search high-quality images...">
+                </div>
                 <button type="button" class="btn abt_searchbtn"><img src="./img/searchwhite.svg">Search</button>
             </div>
         </div>
@@ -19,7 +28,7 @@
 </section>
 <!-- ABOUT S1 End -->
 
-<!-- ABOUT S1 Start -->
+<!-- ABOUT S2 Start -->
 <section class="abt_s2">
     <div class="abt_c2 container">
         <img src="./img/abt_s1leftline.png">
@@ -30,6 +39,32 @@
         <img src="./img/abt_s1rightline.png">
     </div>
 </section>
-<!-- ABOUT S1 End -->
+<!-- ABOUT S2 End -->
+
+<!-- ABOUT S3 Start -->
+<section class="abt_s3">
+    <div class="abt_c3 container">
+        <img class="desktop_none" src="./img/abt_s3img.png">
+        <div class="abt_s3left">
+            <h2 class="abt_s3title">How&nbsp;<span class="abt_s3titlewhite">we do it</span></h2>
+            <p class="abt_s3subtitle">We partner with talented photographers and artists worldwide to curate a diverse selection of stock images. Our stringent quality checks ensure that only the best visuals make it into our library.<br><br>
+            Utilizing advanced search and filtering tools, we make it simple for you to find and download the perfect images, all while offering exceptional customer service.</p>
+        </div>
+    </div>
+</section>
+<!-- ABOUT S3 End -->
+
+<!-- ABOUT S4 Start -->
+<section class="abt_s4">
+    <div class="abt_c4 container">
+        <img class="desktop_none" src="./img/abt_s4img.png">
+        <div class="abt_s3right">
+            <h2 class="abt_s3title">why&nbsp;<span class="abt_s3titlewhite">we do it</span></h2>
+            <p class="abt_s3subtitle">We believe that high-quality visuals are essential for effective communication and creativity. By providing easy access to premium stock images, we empower businesses, designers, and content creators to enhance their projects.<br><br>
+            Our mission is to simplify the process of finding and using outstanding visuals, enabling our customers to focus on what they do best.</p>
+        </div>
+    </div>
+</section>
+<!-- ABOUT S3 End -->
 
 <?php include 'includes/footer.php'; ?>
