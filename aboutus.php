68f1b34740eec38649b067e34fdbb132f88ddc06
SUPPORT HIVES <supporthives@SUPPORTs-MacBook-Air.local>
2026-06-17 06:23:15 +0530

    rahul

---
+++ aboutus.php
@@ -0,0 +1,35 @@
+<?php include 'includes/header.php'; ?>
+
+<!-- ABOUT S1 Start -->
+<section class="abt_s1">
+    <div class="abt_c1 container">
+        <div class="abt_s1left">
+            <div class="abt_s1titlebar">
+                <h2 class="abt_s1title">About Us</h2>
+                <p class="abt_s1subtitle">We simplify the search for high-quality stock images. Our curated library, created by talented global contributors, offers diverse visuals to meet your needs. We are committed to affordable, royalty-free images and exceptional customer service.</p>
+            </div>
+            <div class="searchboxmain">
+                <img class="search_img" src="./img/search.svg">
+                <input type="email" class="form-control abt_s1textbox" id="" placeholder="Search high-quality images...">
+                <button type="button" class="btn abt_searchbtn"><img src="./img/searchwhite.svg">Search</button>
+            </div>
+        </div>
+        <div class="abt_s1right"></div>
+    </div>
+</section>
+<!-- ABOUT S1 End -->
+
+<!-- ABOUT S1 Start -->
+<section class="abt_s2">
+    <div class="abt_c2 container">
+        <img src="./img/abt_s1leftline.png">
+        <div class="abt_s2titlebar">
+            <h3 class="abt_s2title">What we do</h3>
+            <p class="abt_s2subtitle">At ImageVault, we provide a vast library of high-quality, royalty-free stock images to meet your creative needs. Our curated collections span diverse categories, ensuring you find the perfect visual for any project. We offer flexible pricing plans and a user-friendly platform, making it easy to access and download premium images quickly and efficiently.</p>
+        </div>
+        <img src="./img/abt_s1rightline.png">
+    </div>
+</section>
+<!-- ABOUT S1 End -->
+
+<?php include 'includes/footer.php'; ?>
